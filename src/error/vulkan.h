5fe7536bf67e432e6eef0ba16c731738a6044f43
Adrien Jeser <adrien@jeser.me>
2016-09-19 20:58:09 +0200

    UTF-8 for Vulkan error

--- src/error/vulkan.h
+++ src/error/vulkan.h
@@ -72,106 +72,109 @@ class Vulkan final : public ae::Error {
     static constexpr const char *ToString(const VkResult code) noexcept {
         switch (code) {
             case VK_SUCCESS:
-                return "Vulkan : Command successfully completed";
+                return u8"Vulkan : Command successfully completed";
                 break;
             case VK_NOT_READY:
-                return "Vulkan : A fence or query has not yet completed";
+                return u8"Vulkan : A fence or query has not yet completed";
                 break;
             case VK_TIMEOUT:
-                return "Vulkan : A wait operation has not completed in the "
+                return u8"Vulkan : A wait operation has not completed in the "
                        "specified time";
                 break;
             case VK_EVENT_SET:
-                return "Vulkan : An event is signaled";
+                return u8"Vulkan : An event is signaled";
                 break;
             case VK_EVENT_RESET:
-                return "Vulkan : An event is unsignaled";
+                return u8"Vulkan : An event is unsignaled";
                 break;
             case VK_INCOMPLETE:
-                return "Vulkan : A return array was too small for the result";
+                return u8"Vulkan : A return array was too small for the result";
                 break;
 
             case VK_ERROR_OUT_OF_HOST_MEMORY:
-                return "Vulan : A host memory allocation has failed";
+                return u8"Vulan : A host memory allocation has failed";
                 break;
             case VK_ERROR_OUT_OF_DEVICE_MEMORY:
-                return "Vulkan : A device memory allocation has failed";
+                return u8"Vulkan : A device memory allocation has failed";
                 break;
             case VK_ERROR_INITIALIZATION_FAILED:
-                return "Vulkan : Initialization of an object could not be "
+                return u8"Vulkan : Initialization of an object could not be "
                        "completed";
                 break;
             case VK_ERROR_DEVICE_LOST:
-                return "Vulkan : The logical or physical device has been lost";
+                return u8"Vulkan : The logical or physical device has been "
+                       u8"lost";
                 break;
             case VK_ERROR_MEMORY_MAP_FAILED:
-                return "Vulkan : Mapping of a memory object has failed";
+                return u8"Vulkan : Mapping of a memory object has failed";
                 break;
             case VK_ERROR_LAYER_NOT_PRESENT:
-                return "Vulkan: A requested layer is not present or could not "
+                return u8"Vulkan: A requested layer is not present or could "
+                       u8"not "
                        "be loaded";
                 break;
             case VK_ERROR_EXTENSION_NOT_PRESENT:
-                return "Vulkan : A requested extension is not supported";
+                return u8"Vulkan : A requested extension is not supported";
                 break;
             case VK_ERROR_FEATURE_NOT_PRESENT:
-                return "Vulkan : A requested feature is not supported";
+                return u8"Vulkan : A requested feature is not supported";
                 break;
             case VK_ERROR_INCOMPATIBLE_DRIVER:
-                return "Vulkan : The requested version of Vulkan is not "
+                return u8"Vulkan : The requested version of Vulkan is not "
                        "supported by the driver or is otherwise incompatible";
                 break;
             case VK_ERROR_TOO_MANY_OBJECTS:
-                return "Vulkan : Too many objects of the type have already "
+                return u8"Vulkan : Too many objects of the type have already "
                        "been created";
                 break;
             case VK_ERROR_FORMAT_NOT_SUPPORTED:
-                return "Vulkan : A requested format is not supported on this "
+                return u8"Vulkan : A requested format is not supported on this "
                        "device";
                 break;
             case VK_ERROR_FRAGMENTED_POOL:
-                return "Vulkan : A requested pool allocation has failed due to "
+                return u8"Vulkan : A requested pool allocation has failed due "
+                       u8"to "
                        "fragmentation of the pool’s memory";
                 break;
 
             /* Extra */
             case VK_ERROR_VALIDATION_FAILED_EXT:
-                return "Vulkan : VK_ERROR_VALIDATION_FAILED_EXT";
+                return u8"Vulkan : VK_ERROR_VALIDATION_FAILED_EXT";
                 break;
 
             case VK_RESULT_RANGE_SIZE:
-                return "Vulkan : VK_RESULT_RANGE_SIZE";
+                return u8"Vulkan : VK_RESULT_RANGE_SIZE";
                 break;
 
             case VK_SUBOPTIMAL_KHR:
-                return "Vulkan : VK_SUBOPTIMAL_KHR";
+                return u8"Vulkan : VK_SUBOPTIMAL_KHR";
                 break;
 
             case VK_RESULT_MAX_ENUM:
-                return "Vulkan : VK_RESULT_MAX_ENUM";
+                return u8"Vulkan : VK_RESULT_MAX_ENUM";
                 break;
 
             /* KHR */
             case VK_ERROR_INCOMPATIBLE_DISPLAY_KHR:
-                return "Vulkan : VK_ERROR_INCOMPATIBLE_DISPLAY_KHR";
+                return u8"Vulkan : VK_ERROR_INCOMPATIBLE_DISPLAY_KHR";
                 break;
             case VK_ERROR_NATIVE_WINDOW_IN_USE_KHR:
-                return "Vulkan : VK_ERROR_NATIVE_WINDOW_IN_USE_KHR";
+                return u8"Vulkan : VK_ERROR_NATIVE_WINDOW_IN_USE_KHR";
                 break;
             case VK_ERROR_SURFACE_LOST_KHR:
-                return "Vulkan : VK_ERROR_SURFACE_LOST_KHR";
+                return u8"Vulkan : VK_ERROR_SURFACE_LOST_KHR";
                 break;
             case VK_ERROR_OUT_OF_DATE_KHR:
-                return "Vulkan : VK_ERROR_OUT_OF_DATE_KHR";
+                return u8"Vulkan : VK_ERROR_OUT_OF_DATE_KHR";
                 break;
 
             /* Nvidia */
             case VK_ERROR_INVALID_SHADER_NV:
-                return "Vulkan : VK_ERROR_INVALID_SHADER_NV";
+                return u8"Vulkan : VK_ERROR_INVALID_SHADER_NV";
                 break;
         }
 
-        return "Vulkan : Unknow error";
+        return u8"Vulkan : Unknow error";
     }
 
    private:
